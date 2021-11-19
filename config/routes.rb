fc3bc1ad95343128b6e8319e53e3c27cfe18baf2
Diego <dalt99144@gmail.com>
2021-11-19 08:02:41 -0600

    Routing for clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'clients', to: 'clients#index'
+      get 'clients/:id', to: 'clients#show'
+      post 'clients/create'
+      patch 'clients/update'
+      delete 'clients/:id', to: 'clients#destroy'
+    end
+  end
+  
+  
   root 'clients#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
