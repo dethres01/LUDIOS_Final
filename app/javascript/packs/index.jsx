1472c694e173e24d9de3eb24fd38961f5b3d08ff
Diego <dalt99144@gmail.com>
2021-11-30 01:19:49 -0600

    es la una de la mañana por favor heroku

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,13 +2,13 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/app'
 import Navigation from '../components/misc/navbar/navbar'
 import { BrowserRouter } from 'react-router-dom'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
