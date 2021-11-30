3cc022c6779e14064facaaeee2e1df6afc5dde36
Diego <dalt99144@gmail.com>
2021-11-30 01:02:36 -0600

    :(

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,11 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+//import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "bootstrap"
