7238b698fa1eeef33e0bcda2baa06e28d1c33dfe
Diego <dalt99144@gmail.com>
2021-11-30 00:21:56 -0600

    uh

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,12 +2,12 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/app'
 import Navigation from '../components/misc/navbar/navbar'
 import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
